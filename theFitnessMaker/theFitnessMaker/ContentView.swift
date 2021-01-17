d064c08f6cb98332a1d9d470ce80b50858efebcf
Onizuka <alexander.nys@hotmail.com>
2021-01-17 19:32:21 +0100

    Second Draft

--- theFitnessMaker/theFitnessMaker/ContentView.swift
+++ theFitnessMaker/theFitnessMaker/ContentView.swift
@@ -11,35 +11,110 @@ import SwiftUI
 @available(iOS 13.0, *)
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Image("fitness-workout")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(height:300)
-            
-            
-            //Vertical Stack - where objects are stacked vertically
+        NavigationView {
             VStack {
+                ZStack {
+                    Image("fitness-workout")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(height:300)
+                    
+                    
+                    //Vertical Stack - where objects are stacked vertically
+                    VStack {
+                        
+                        Spacer()
+                        
+                        Text("Full Body Workout")
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        Text("For Beginners")
+                            .fontWeight(.regular)
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                    .frame(width: 380)
+                    .background(Color(#colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 0.5)))
+                }
+                .frame(width:380, height: 300)
+                .cornerRadius(20)
+                .clipped()
+                .shadow(radius: 8)
+                .padding(.top,20)
                 
-                Spacer()
-                
-                Text("Full Body Workout")
+                Text("Weekly Plan")
                     .font(.largeTitle)
                     .fontWeight(.semibold)
-                    .foregroundColor(.white)
-                Text("For Beginners")
-                    .fontWeight(.regular)
-                    .foregroundColor(.white)
+                    .padding()
+                // Stack where the inner elements are arranged/stacked horizontally
+                HStack {
+                    
+                    // day card
+                    ZStack {
+                        Image("push-ups")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(height:220)
+                        
+                        //Vertical Stack - where objects are stacked vertically
+                        VStack {
+                            
+                            Spacer()
+                            
+                            Text("Monday")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.white)
+                            Text("Chest")
+                                .fontWeight(.regular)
+                                .foregroundColor(.white)
+                        }
+                        .padding()
+                        .frame(width: 150)
+                        .background(Color(#colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 0.5)))
+                        
+                    }
+                    .frame(width:150, height: 220)
+                    .clipped()
+                    .cornerRadius(20)
+                    .shadow(radius: 8)
+                    
+                    // day card
+                    ZStack {
+                        Image("push-ups")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(height:220)
+                        
+                        //Vertical Stack - where objects are stacked vertically
+                        VStack {
+                            
+                            Spacer()
+                            
+                            Text("Monday")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.white)
+                            Text("Chest")
+                                .fontWeight(.regular)
+                                .foregroundColor(.white)
+                        }
+                        .padding()
+                        .frame(width: 150)
+                        .background(Color(#colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 0.5)))
+                        
+                    }
+                    .frame(width:150, height: 220)
+                    .clipped()
+                    .cornerRadius(20)
+                    .shadow(radius: 8)
+                }
+                
+                Spacer()
             }
-            .padding()
-            .frame(width: 380)
-            .background(Color(#colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 0.5)))
+            .navigationBarTitle("Home Workouts")
         }
-        .frame(width:380, height: 300)
-        .cornerRadius(20)
-        .clipped()
-        .shadow(radius: 8)
-        .padding(.top,20)
     }
 }
 
